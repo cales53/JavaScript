057c4861ff36f57adbf1e830464d84f39a70c132
Carlos Vega <cales53@gmail.com>
2020-11-26 18:40:36 -0500

    se agrego html, sql y javascript files

---
+++ JS.js
@@ -0,0 +1,15 @@
+function myFunction(p)
+{
+  var x = prompt("Cual es tu nombre");
+  //var p = 0;
+  //p = p.value;
+  if (p > 90)
+  {
+    console.log("gordo");
+  }
+  else
+  {
+      console.log("vas bien");
+  }
+  alert("hola " + x + " tienes un peso de " + p);
+}
